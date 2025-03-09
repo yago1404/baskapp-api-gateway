56333b4562e7f3a9ba1efe477be2c3f7bb35431b
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2025-03-09 19:27:31 -0300

    feat: create http client

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import * as dotenv from 'dotenv';
 import { LogMiddleware } from './infra/middlewares/log.middleware';
 import { UserModule } from './modules/user/user.module';
+import { HttpClientService } from './infra/http/http_client.service';
 
 dotenv.config();
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HttpClientService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): any {
