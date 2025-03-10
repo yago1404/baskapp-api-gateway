1f06e487a7e8d70c4527e41fcd58646400e032a7
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2025-03-10 17:02:02 -0300

    fix: remove unnecessary service

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-import { UserService } from './user.service';
 import { InternalClientService } from '../../infra/http/internal_client_service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, InternalClientService],
+  providers: [InternalClientService],
 })
 export class UserModule {}
